1312893026ab4a7c91d04ac43da5d1acace20d56
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2016-03-31 20:43:12 -0400

    Ran Code Cleanup.

--- FrancescoMagliocco/Utils/Properties/AssemblyInfo.cs
+++ FrancescoMagliocco/Utils/Properties/AssemblyInfo.cs
@@ -1,10 +1,35 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// ----------------------------------------------------------------------------
+// <Copyright File="AssemblyInfo.cs" Company="FrancescoMagliocco"
+//     Copyright (C) 2016 FrancescoMagliocco
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// <Contact Email="Magliocco.W.Francesco@Gmail.com"/>
+// <Coded Year="2016">
+//     <Month>03</Month>
+//     <Day>31</Day>
+//     <Time>15:06</Time>
+// </Coded>
+// ----------------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("LibUtils")]
 [assembly: AssemblyDescription("A Library containing useful Utilities to make the life of a Coder easier.")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +42,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("5e64c582-ce81-4bc4-b116-df2327ae800e")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +59,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.*")]
 ////[assembly: AssemblyFileVersion("1.0.0.0")]
